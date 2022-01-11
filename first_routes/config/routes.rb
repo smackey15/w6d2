3be6fb3cb9b8cc212c67e3b10433de52a1e8db23
avakil3 <aagamvakil@hotmail.com>
2022-01-11 14:00:00 -0500

    first commit

---
+++ first_routes/config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # resources :users
+	get 'users', to: 'users#index'
+	post 'users', to: 'users#create'
+	get 'users/new', to: 'users#new', as: 'new_user'
+
+
+
+end
