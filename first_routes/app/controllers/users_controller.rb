9e7382848465fef0c50a1e817f24cc626cb8452a
sean <midruss@yahoo.com>
2022-01-11 17:21:54 -0500

    completed all methods in the user controller

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -7,13 +7,33 @@ class UsersController < ApplicationController
 
     def create
        user = User.new(params.require(:user).permit(:name, :email))
-       user.save!
-       render json: user
+       if user.save
+        render json: user
+      else
+        render json: user.errors.full_messages, status: :unprocessable_entity
+      end
     end
 
     def show
-        render json: params
+        @user = User.find(params[:id])
+        render json: @user
     end
 
+    def update
+        user = User.find(params[:id])
+        if user.update(params.require(:user).permit(:name, :email))
+            redirect_to user_url(user.id) ##what does user_url do
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
 
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        redirect_to user_url
+    end
 end
+
+#how do database entires get stored/shared bewteen machines when we are pairing?
+#is there more than one way to test show, aside from typing in the url?
