aa922b707ad46c9456e89d0a07de8801e424af34
avakil3 <aagamvakil@hotmail.com>
2022-01-12 14:48:08 -0500

    re-committed

--- first_routes/app/models/artwork_share.rb
+++ first_routes/app/models/artwork_share.rb
@@ -1,6 +1,15 @@
 class ArtworkShare < ApplicationRecord
     validates :viewer_id, uniqueness: { scope: :artwork_id }
 
+    belongs_to :viewer,
+        primary_key: :id,
+        foreign_key: :viewer_id,
+        class_name: :User
+    
+    belongs_to :artwork,
+        primary_key: :id,
+        foreign_key: :artwork_id,
+        class_name: :Artwork
  
 
 end
